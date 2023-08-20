7a546ce2a68e3766a62ada1b8b573e79aa09b852
fischl05 <jooktkkjoo20@gmail.com>
2023-08-20 15:34:15 +0900

    2023 skill competition
    Task 3 Embedded programing
    change task

---
+++ change_task/made by other my made answer code/washing_machine/Core/Inc/NEXTION.h
@@ -0,0 +1,19 @@
+#ifndef INC_NEXTION_H_
+#define INC_NEXTION_H_
+
+#include "main.h"
+
+typedef struct{
+	uint16_t x;
+	uint16_t y;
+}coordinate;
+
+void NEXTION_puts(char *ID, char *string);
+uint8_t NEXTION_Put_Value_Etc(char *ID, char *attribute, char *value);
+void NEXTION_Change_Page(char *Page);
+uint8_t NEXTION_Get_Touch(uint8_t Page);
+uint8_t NEXTION_Get_Button_Value(uint8_t ID);
+uint8_t NEXTION_Get_Page(uint8_t Page);
+
+#endif /* INC_NEXTION_H_ */
+
